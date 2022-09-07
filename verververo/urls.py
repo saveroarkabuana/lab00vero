1fe0f41733f4076ea3fa47bd72b3c4296246d07a
Savero Arkabuana <savero.arkabuana@gmail.com>
2022-09-07 15:16:02 +0700

    SSSSSSSSAVERO

--- verververo/urls.py
+++ verververo/urls.py
@@ -1,4 +1,4 @@
-"""verververo URL Configuration 
+"""verververo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- verververo/wsgi.py
+++ verververo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for verververo project. 
+WSGI config for verververo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
